87499e99953ceada1eb35d2a89829b6bb0870257
slavablack <veaceslav.ciornii@magenta.de>
2021-12-17 10:21:06 +0100

    button funktioniert

--- index.js
+++ index.js
@@ -1,9 +1,13 @@
 let citate=[];
 citate.push("Christmas will always be as long as we stand heart to heart and hand in hand. ")
-
+citate.push("test");
+citate.push("tesztutzt");
 
 
 function clickfunction () {
+    console.log(citate)
+    const rand = Math.floor(Math.random() * citate.length);
+
+    document.getElementById ("para").innerHTML = citate[rand];
     
-    document.getElementById ("santa-img").innerHTML = citate;
 }
